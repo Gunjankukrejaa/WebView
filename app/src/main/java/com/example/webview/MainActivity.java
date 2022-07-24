a6f96c6f800872d18844e5eb8e068f25e73bd5b4
Gunjankukrejaa <gkukreja233@gmail.com>
2022-07-23 22:34:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+        
+        
+        
+    }
+}
